a6a7167077d2d219fafe54839884b9fb3b474118
nhennigan <niamhhennigan@gmail.com>
2021-03-22 16:05:30 +0000

    seating chart code tidied up

--- backend/main.py
+++ backend/main.py
@@ -298,69 +298,6 @@ def home_page():
 #        return render_template('admin_home.html',user = admin)
 #    return render_template('home.html',user=user_in,exams=exam_list)
 
-@server.route('/seating_chart',methods = ['POST','GET'])
-@login_required
-def plot_seating_chart():
-    mat = []
-    if request.method == 'POST' and 'venue' in request.form and 'seat_no' in request.form:
-#        seating_chart.plot_seating(request.form['venue'],request.form['seat_no'])
-        kingfisher_length =3
-        kingfisher_breadth = 4
-        bailey_allen_length = 20
-        bailey_allen_breadth = 30
-        engineering_length = 10
-        engineering_breadth = 4
-        galway_bay_length = 8
-        galway_bay_breadth = 10
-        leisureland_length = 5
-        leisureland_breadth = 10
-
-        if request.form['venue'] == "Kingfisher":
-            length = kingfisher_length
-            breadth = kingfisher_breadth
-        elif request.form['venue'] == "Bailey":
-            length = bailey_allen_length
-            breadth = bailey_allen_breadth
-        elif request.form['venue'] == "Alice":
-            length = engineering_length
-            breadth = engineering_breadth
-        elif request.form['venue'] == "Galway":
-            length = galway_bay_length
-            breadth = galway_bay_breadth
-        else:
-            length = leisureland_length
-            breadth = leisureland_breadth
- 
-        mat = []
-        for i in range(length):
-            rowList = []
-            for j in range(breadth):
-                # you need to increment through dataList here, like this:
-                rowList.append('clear_chair.PNG')
-            mat.append(rowList)
- 
-    counter=0
-    x_count=-1
-    for x in mat:
-        x_count +=1
-        y_count=-1
-        for y in x:
-            y_count+=1
-            counter +=1
-            if counter == int(request.form['seat_no']):
-                mat[x_count][y_count]='highlighted_chair.png'
-    
-    mat_t = list(zip(*mat))
-
-    f_name,l_name,course = conn.get_student_data(session["username"])
-    exam_list = conn.get_exam_data(session["username"])
-    user_in = User(session["username"],f_name,l_name,course)
-#    return mat_t   
-    #return render_template('home.html',user=user_in,exams=exam_list,m=mat_t)
-#    return redirect(url_for('home_page',seating = mat_t))
-    return render_template('seat.html', m= mat_t,v = "")
-#   return redirect(url_for(home_page))
-
 
 @server.route('/admin_home/', methods = ['POST','GET'])
 @login_required
